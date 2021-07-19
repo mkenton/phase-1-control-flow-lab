ed391a7080a2b038d65cb28719b089385781c758
Michael <mkenton2020@icloud.com>
2021-07-18 23:56:26 -0500

    commit tree f1418ac7c218dc0a8c27a54dd4da44c5c382ef21

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(rideFeet){
+  if (rideFeet <= 400) {
+    return "This one is on me!";
+  }
+  else if (rideFeet > 400 && rideFeet <=2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (rideFeet > 2500) {
+    return "No can do."
+  }
+}
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(city){
+  const message = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous": 
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default: return "Bye."
+  }
 }
